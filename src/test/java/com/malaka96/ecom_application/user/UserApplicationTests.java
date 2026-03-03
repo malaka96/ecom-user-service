2e20131410a65693e47166eef95b116931ae8345
malaka96 <madhubhashana655@gmail.com>
2026-03-03 20:50:22 +0530

    initial commit

---
+++ src/test/java/com/malaka96/ecom_application/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malaka96.ecom_application.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
